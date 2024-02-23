2c4872e4d7107e8fed1fc81c36b3de9a30460b3a
Irfan Saf <irfansaf7@gmail.com>
2024-02-23 17:53:20 +0700

    fix: Dashboard Events

--- src/app/(main)/dashboard/events/page.tsx
+++ src/app/(main)/dashboard/events/page.tsx
@@ -1,52 +1,18 @@
-import EventStatusDashboard from "@/components/event/EventStatusDashboard";
 import React from "react";
+import RegisteredEvents from "@/app/(main)/dashboard/events/_components/RegisteredEvents";
 
 export default function Page() {
+
    return (
       <div className="container mx-auto px-4 ">
          <div className="mt-6 flex flex-col bg-white">
             <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                  <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
-                     <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                        <thead className="bg:transparent dark:bg-gray-800">
-                           <tr className="text-center font-[500] text-[#353535]">
-                              <th scope="col" className="px-4 py-3.5 ">
-                                 Events Name
-                              </th>
-
-                              <th scope="col" className="px-12 py-3.5">
-                                 Organization
-                              </th>
-
-                              <th scope="col" className="py-3.5">
-                                 Status
-                              </th>
-
-                              <th scope="col" className="py-3.5">
-                                 Action
-                              </th>
-                           </tr>
-                        </thead>
-
-                        <tbody className="divide-y divide-gray-200">
-                           <tr>
-                              <td className="whitespace-nowrap px-4 py-3.5 text-center">
-                                 Compsphere
-                              </td>
-                              <td className="whitespace-nowrap py-3.5 text-center">
-                                 PUFA Computing
-                              </td>
-                              <td className="whitespace-nowrap py-3.5 text-center">
-                                 <EventStatusDashboard status="on progress" />
-                              </td>
-                              <td className="whitespace-nowrap py-3.5 text-center">
-                                 <button className="text-[#02ABF3] font-normal">Edit</button>
-                              </td>
-                           </tr>
-                        </tbody>
-                     </table>
-                  </div>
+                  {/*Title List of event registered*/}
+                  <h1 className="text-2xl font-semibold text-[#353535] text-center">
+                     List of Event Registered
+                  </h1>
+                  <RegisteredEvents />
                </div>
             </div>
          </div>
--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { access } from "fs";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import Footer from "@/components/Footer";
 
 export default function DashboardLayout({
    children,
@@ -61,9 +62,9 @@ useEffect(() => {
                <path
                   d="M3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V9C21 7.89543 20.1046 7 19 7H13L11 5H5C3.89543 5 3 5.89543 3 7Z"
                   stroke="#9CA3AF"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                />
             </svg>
          ),
@@ -82,9 +83,9 @@ useEffect(() => {
                <path
                   d="M8 7V3M16 7V3M7 11H17M5 21H19C20.1046 21 21 20.1046 21 19V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V19C3 20.1046 3.89543 21 5 21Z"
                   stroke="#9CA3AF"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                />
             </svg>
          ),
@@ -103,9 +104,9 @@ useEffect(() => {
                <path
                   d="M20 13V6C20 4.89543 19.1046 4 18 4H6C4.89543 4 4 4.89543 4 6V13M20 13V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18V13M20 13H17.4142C17.149 13 16.8946 13.1054 16.7071 13.2929L14.2929 15.7071C14.1054 15.8946 13.851 16 13.5858 16H10.4142C10.149 16 9.89464 15.8946 9.70711 15.7071L7.29289 13.2929C7.10536 13.1054 6.851 13 6.58579 13H4"
                   stroke="#9CA3AF"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                />
             </svg>
          ),
@@ -124,9 +125,9 @@ useEffect(() => {
                <path
                   d="M9 19V13C9 11.8954 8.10457 11 7 11H5C3.89543 11 3 11.8954 3 13V19C3 20.1046 3.89543 21 5 21H7C8.10457 21 9 20.1046 9 19ZM9 19V9C9 7.89543 9.89543 7 11 7H13C14.1046 7 15 7.89543 15 9V19M9 19C9 20.1046 9.89543 21 11 21H13C14.1046 21 15 20.1046 15 19M15 19V5C15 3.89543 15.8954 3 17 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21H17C15.8954 21 15 20.1046 15 19Z"
                   stroke="#9CA3AF"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                />
             </svg>
          ),
@@ -153,6 +154,10 @@ useEffect(() => {
             </div>
          </aside>
          <main className="ml-[200px] p-3">{children}</main>
+
+         {/*exclude Footer component from the layout*/}
+
+
       </div>
    );
 }
