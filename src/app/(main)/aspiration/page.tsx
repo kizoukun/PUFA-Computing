fd41f6a89779c8646238e3ed3d37fe9a642b22ab
Alden Sayidina <aldraddd01@gmail.com>
2024-02-26 21:26:42 +0700

    added a toggle switch on inspiration box

--- src/app/(main)/aspiration/page.tsx
+++ src/app/(main)/aspiration/page.tsx
@@ -50,9 +50,17 @@ export default function page() {
 
                <div className="mb-6 flex flex-col gap-2">
                   <h1 className="text-[1.1rem] font-bold">From:</h1>
-                  <p className="text-[0.9rem]">
-                     Share it anonymously (insert toggle button here)
-                  </p>
+                  <div className="flex gap-2">
+                     <p className="text-[0.9rem]">
+                        Share it anonymously 
+                     </p>
+                     <label className="inline-flex items-center cursor-pointer">
+                        <input type="checkbox" value="" className="sr-only peer"/>
+                        <div className="scale-90 relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+                        <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"/>
+                     </label>
+                  </div>
+
                   <input
                      type="text"
                      className="w-[100%] rounded-lg border-2 p-2"
