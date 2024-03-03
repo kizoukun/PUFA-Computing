f629b6e095040c7ece1990f0327254f4a00c460f
kizoukun <kizoukungaming@gmail.com>
2024-03-03 14:51:40 +0700

    feat(aspiration): create aspiration now working properly

---
+++ src/app/(main)/aspiration/_components/Aspirations.tsx
@@ -0,0 +1,41 @@
+import AspirationCard from "@/components/aspiration/AspirationCard";
+import { db } from "@/lib/db";
+
+export default async function AspirationsCards() {
+   const aspiration = await db.aspiration.findMany({
+      select: {
+         id: true,
+         to: true,
+         from: true,
+         anonymous: true,
+         subject: true,
+         message: true,
+         createdAt: true,
+         AspirationLike: {
+            select: {
+               id: true,
+            },
+         },
+      },
+      orderBy: {
+         createdAt: "desc",
+      },
+   });
+
+   //TODO: Handle likes based on user loggedin
+
+   return (
+      <div className="grid w-full grid-cols-1 gap-8 py-4 md:grid-cols-2">
+         {aspiration.map((asp) => (
+            <AspirationCard
+               key={asp.id}
+               title={asp.subject}
+               date={asp.createdAt}
+               from={asp.from}
+               to={asp.to}
+               message={asp.message}
+            />
+         ))}
+      </div>
+   );
+}
--- src/components/aspiration/AspirationCard.tsx
+++ src/components/aspiration/AspirationCard.tsx
@@ -2,7 +2,21 @@
 import React, { useState } from "react";
 import { IoIosHeartEmpty, IoIosHeart } from "react-icons/io";
 
-export default function AspirationCard() {
+type AspirationCardProps = {
+   title: string;
+   message: string;
+   from: string;
+   to: string;
+   date: Date;
+};
+
+export default function AspirationCard({
+   title,
+   message,
+   from,
+   to,
+   date,
+}: AspirationCardProps) {
    const [showAdminReply, setShowAdminReply] = useState(false);
 
    // Handler untuk menampilkan atau menyembunyikan balasan admin
@@ -15,12 +29,31 @@ export default function AspirationCard() {
    const toggleLike = () => {
       setLiked(!liked);
    };
+
+   const dateConvert = (date: Date) => {
+      const months = [
+         "Jan",
+         "Feb",
+         "Mar",
+         "Apr",
+         "May",
+         "Jun",
+         "Jul",
+         "Aug",
+         "Sep",
+         "Oct",
+         "Nov",
+         "Dec",
+      ];
+      return `${date.getDate().toString().padStart(2, "0")} ${
+         months[date.getMonth()]
+      } ${date.getFullYear().toString().padStart(2, "0")}`;
+   };
+
    return (
       <div className="flex flex-col gap-4 rounded-lg border-2 border-gray-300 p-4 md:p-6">
          <div className="flex flex-row justify-between">
-            <h1 className="text-lg font-bold capitalize md:text-xl">
-               Enrollment sdasd
-            </h1>
+            <h1 className="text-lg font-bold capitalize md:text-xl">{title}</h1>
             <button
                onClick={toggleAdminReply}
                className={`md:ml-auto ${
@@ -36,18 +69,15 @@ export default function AspirationCard() {
                >
                   <path
                      id="Icon"
-                     fill-rule="evenodd"
-                     clip-rule="evenodd"
+                     fillRule="evenodd"
+                     clipRule="evenodd"
                      d="M0.351472 0.390524C0.820101 -0.130175 1.5799 -0.130175 2.04853 0.390524L6 4.78105L9.95147 0.390524C10.4201 -0.130175 11.1799 -0.130175 11.6485 0.390524C12.1172 0.911223 12.1172 1.75544 11.6485 2.27614L6.84853 7.60948C6.3799 8.13017 5.6201 8.13017 5.15147 7.60948L0.351472 2.27614C-0.117157 1.75544 -0.117157 0.911223 0.351472 0.390524Z"
                      fill="#6B7280"
                   />
                </svg>
             </button>
          </div>
-         <p className="text-[16px] font-[400] text-[#6B7280]">
-            Masa gua udah enroll tp ga di approve” coo masa harus samperin
-            dosennya lgi sihh yang bener aja rugi dongg
-         </p>
+         <p className="text-[16px] font-[400] text-[#6B7280]">{message}</p>
          {showAdminReply && (
             <p className="text-[16px] font-[400] text-[#111827]">
                {" "}
@@ -60,13 +90,11 @@ export default function AspirationCard() {
             </p>
          )}
          <div className="flex flex-col">
-            <p className="text-[14px] font-semibold text-[#111827]">
-               Anonymous
-            </p>
+            <p className="text-[14px] font-semibold text-[#111827]">{from}</p>
             <div className="flex flex-col gap-2 text-sm md:flex-row md:justify-between">
                <div className="text-[#6B7280]">
                   <p>
-                     To: Puma IT <span> · </span> <span>Feb 14, 3099</span>
+                     To: {to} <span> · </span> <span>{dateConvert(date)}</span>
                   </p>
                </div>
                <div className="flex gap-2 text-sm">
