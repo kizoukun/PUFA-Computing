27b1e51e7ed1c51dc9121803edddafd6af5cb26a
Alden Sayidina <aldraddd01@gmail.com>
2024-03-06 15:36:23 +0700

    added payment/pay section

--- src/app/(main)/merch/payment/pay/page.tsx
+++ src/app/(main)/merch/payment/pay/page.tsx
@@ -3,61 +3,136 @@ import React from "react";
 
 export default function page() {
    return (
-      <div>
-         <div className="bg-[#F2F2F2] p-10">
-            <div className="mx-auto flex max-w-7xl flex-col border-l-4 border-[#BA704F] pl-5 ">
-               <div className="flex items-center">
-                  <p className="text-[35px] font-bold text-[#353535]">
-                     Computing Store
-                  </p>
-               </div>
-               <p className="text-[12px] font-[400] text-[#353535]">
+      <>
+         {/* header title  */}
+         <header className="bg-gray-200 px-[10rem] py-10">
+            <div className="border-l-4 border-orange-500 pl-4">
+               <h1 className="text-[2rem] font-bold">Computing Store</h1>
+               <p className="text-[0.8rem]">
                   Special merchandise for all Computizens
                </p>
             </div>
-         </div>
+         </header>
 
-         <div className="mx-auto max-w-7xl pt-6 text-[#353535]">
-            <p className="text-[24px] font-[400]">
-               Great Choices, Muhammad Ilham!
-            </p>
-            <p className="text-[26px] font-[600]">
-               Let's finish the payment, and the item will arrive soon!
-            </p>
-         </div>
-         <div className="py-6">
-            <Seperator className="mx-auto max-w-7xl border-[#D1D5DB]" />
-         </div>
-         <div>
-            <p className="mx-auto max-w-7xl text-[24px] font-[500] text-[#353535]">
-               Details Order
-            </p>
-            <p className="mx-auto max-w-7xl text-[16px] font-[500] text-[#6B7280]">
-               Details of Your Order
-            </p>
+         <section className="space-y-12 px-[10rem] py-10">
+            <div className="">
+               <h1 className="text-[1.2rem]">Great Choices, Computizen!</h1>
+               <h1 className="text-[1.2rem] font-bold">
+                  Let's finish up the payment and your item(s) will arrive soon!
+               </h1>
+            </div>
 
-            <div className="grid grid-cols-2">
+            <hr className="h-[2px] bg-gray-200" />
+
+            {/* details order  */}
+            <div className="flex flex-col gap-4">
                <div>
-                  <p>sda</p>
+                  <h1 className="text-[1.2rem] font-bold">Details Order</h1>
+                  <h1 className="text-[0.8rem]">Details of your order(s)</h1>
                </div>
-               <div>
-                  <p className="text-[16px] font-[600] text-[#353535]">
-                     Computing Varsity
-                  </p>
-                  <p className="text-[20px] font-[600] text-[#BA704F]">
-                     Rp. 250.000
-                  </p>
-                  <div className="flex gap-x-2">
-                     <p className="text-[20px] font-[600] text-[#353535]">
-                        Color:
-                     </p>
-                     <label className="justitfy-center flex items-center rounded-2xl border border-[#9DA2A7] px-6 py-1 text-[14px] font-[400] text-[#9DA2A7]">
-                        Black n Grey
-                     </label>
+               <div className="flex gap-4">
+                  <div className="h-[200px] w-[160px] rounded-lg bg-black"></div>
+                  <div className="flex flex-col gap-5">
+                     <h1 className="text-[1.1rem] font-bold">
+                        Computing Varsity
+                     </h1>
+                     <h1 className="text-[1.1rem] font-bold text-orange-500">
+                        Rp. 250,000
+                     </h1>
+                     <div className="flex items-center gap-4">
+                        <h1>Color:</h1>
+                        <button className="rounded-xl border border-gray-400 px-4 py-1 text-[0.8rem] text-gray-400">
+                           Black n Grey
+                        </button>
+                     </div>
+                     <div className="flex items-center gap-4">
+                        <h1>Size:</h1>
+                        <button className="ml-3 rounded-xl border border-gray-400 px-4 py-1 text-[0.8rem] text-gray-400">
+                           XL
+                        </button>
+                     </div>
                   </div>
                </div>
             </div>
-         </div>
-      </div>
+
+            {/* notes textbox */}
+            <div>
+               <input
+                  type="text"
+                  name=""
+                  id=""
+                  className="w-[50%] rounded-lg border-2 border-gray-200 p-2"
+                  placeholder="Notes"
+               />
+            </div>
+
+            {/* address info */}
+            <div className="flex flex-col gap-2">
+               <h1>Address</h1>
+               <h1>Let us know where to send your order(s)</h1>
+               <input
+                  type="text"
+                  name=""
+                  id=""
+                  className="w-full rounded-lg border-2 border-gray-200 p-2"
+                  placeholder="SBH, G-444"
+               />
+            </div>
+
+            {/* payment method  */}
+            <div className="flex flex-col gap-10">
+               <div>
+                  <h1 className="text-[1.2rem] font-bold">Payment Method</h1>
+                  <h1 className="text-[0.8rem] text-gray-400">
+                     Payment can be made via:
+                  </h1>
+               </div>
+
+               <div>
+                  <h1>BCA:</h1>
+                  <h1>6050537330</h1>
+                  <h1>a/n Alexandra Verencia Poluakan</h1>
+               </div>
+
+               <div>
+                  <h1>ShopeePay:</h1>
+                  <h1>081294807325</h1>
+                  <h1>a/n Dian Vania Jessicha</h1>
+               </div>
+
+               <div>
+                  <h1>Line Bank/Hana Bank:</h1>
+                  <h1>183377746330</h1>
+                  <h1>a/n Dinda Ayu Maulina</h1>
+               </div>
+
+               <div>
+                  <h1 className="text-[1.2rem] font-bold">Contact Person</h1>
+                  <h1>0012003483290 (Kevin Marchanda)</h1>
+               </div>
+            </div>
+
+            {/* submit proof box  */}
+            <div className="flex flex-col gap-4">
+               <h1 className="text-[1.2rem] font-bold">
+                  Submit your payment proof!
+               </h1>
+               <button className="border-2 border-dashed bg-white py-20">
+                  <h1 className="text-gray-400">
+                     Click here to upload your payment proof!
+                  </h1>
+                  <h1 className="text-[0.8rem] italic text-gray-400">
+                     Minimum: 1 photo, Maximum: 3 photos
+                  </h1>
+                  <h1 className="text-gray-400">...</h1>
+               </button>
+               <div className="mt-6 flex self-center">
+                  <button className="rounded-lg border-2 border-gray-300 bg-white px-[12rem] py-4 transition-all duration-200 hover:scale-110 hover:shadow-lg">
+                     Submit
+                  </button>
+               </div>
+            </div>
+         </section>
+      </>
    );
 }
