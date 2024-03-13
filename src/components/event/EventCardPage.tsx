a7df1f709a107456a9e14f261c1b054b4d94a3b5
Irfan Saf <irfansaf7@gmail.com>
2024-03-13 11:08:33 +0700

    fix(events): fix src image

--- src/components/event/EventCardPage.tsx
+++ src/components/event/EventCardPage.tsx
@@ -9,7 +9,7 @@ export default function EventCardPage({ events }: { events: Event[] }) {
            <div key={event.id} className="flex gap-4 rounded-[15px] border-[0.5px] border-[#E50D0D] md:gap-8">
              <div>
                <Image
-                  src="/events/default.png"
+                  src={event.thumbnail}
                   className="h-full w-96 scale-110 rounded-[15px]"
                   alt={`${event.title}'s poster`}
                   width={384}
