f105dbbdcabde534a7ab082f7372ba4e32063973
Alden Sayidina <aldraddd01@gmail.com>
2024-03-24 22:02:17 +0700

    pageheading component fixed

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -6,13 +6,21 @@ interface PageHeadingProps {
    borderColor: string;
 }
 
-export default function PageHeading({ title, description }: PageHeadingProps) {
+const PageHeading: React.FC<PageHeadingProps> = ({
+   title,
+   description,
+   borderColor,
+}) => {
+   const borderClass = borderColor ? `border-l-4 border-[${borderColor}]` : "";
+
    return (
       <section className="flex flex-col bg-[#F2F2F2] px-[5rem] py-[2rem] md:px-[10rem]">
-         <div className="border-l-4 border-['${borderColor}'] pl-8">
+         <div className={`${borderClass} pl-8`}>
             <h1 className="text-[2rem] font-[600]">{title}</h1>
             <h1 className="text-[0.8rem]">{description}</h1>
          </div>
       </section>
    );
-}
+};
+
+export default PageHeading;
