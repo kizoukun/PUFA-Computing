6e6825ab667fc5fd05f7d09066a12c65641859c7
Alden Sayidina <aldraddd01@gmail.com>
2024-03-24 21:55:18 +0700

    pageheading component

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -2,17 +2,28 @@ import React from "react";
 
 interface PageHeadingProps {
    title: string;
-   description: string;
-   borderColor: string;
+   description?: string;
+   borderColor?: string;
 }
 
-export default function PageHeading({ title, description }: PageHeadingProps) {
+const PageHeading: React.FC<PageHeadingProps> = ({
+   title,
+   description,
+   borderColor,
+}) => {
+   const borderClass = borderColor ? `border-l-4 border-[${borderColor}]` : "";
+
    return (
       <section className="flex flex-col bg-[#F2F2F2] px-[5rem] py-[2rem] md:px-[10rem]">
-         <div className="border-l-4 border-['${borderColor}'] pl-8">
+         <div className={`${borderClass} pl-8`}>
             <h1 className="text-[2rem] font-[600]">{title}</h1>
-            <h1 className="text-[0.8rem]">{description}</h1>
+            {description && (
+               <h2 className="text-[0.8rem]">{description}</h2>
+            )}{" "}
+            {/* Conditionally render description */}
          </div>
       </section>
    );
-}
+};
+
+export default PageHeading;
