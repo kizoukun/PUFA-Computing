2fba25ba1ab2c3dbb6e7ea31af6c7abdd08c2d92
Irfan Saf <irfansaf7@gmail.com>
2024-03-25 20:48:37 +0700

    fix(dashboard): fixing dashboard event

--- src/services/api/user.tsx
+++ src/services/api/user.tsx
@@ -76,7 +76,7 @@ export async function fetchUserEvents(userId: string): Promise<Event[]> {
             Authorization: `Bearer ${token}`,
          },
       });
-      return response.data.events;
+      return response.data.data;
    } catch (error) {
       console.error("Error fetching user events", error);
       throw error;
