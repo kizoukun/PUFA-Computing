59eaa47aadec9bb72527ac481b35cce46e352c6b
kizoukun <kizoukungaming@gmail.com>
2024-03-29 19:40:13 +0700

    fix(auth): change organization to major, and fix linting

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,91 +6,91 @@ import { z } from "zod";
 const bcrypt = require("bcrypt");
 
 export async function UserRegister(form: unknown) {
-   try {
-      const result = UserRegisterSchema.safeParse(form);
-      if (!result.success) {
-         let errorMessage = "";
-         result.error.issues.forEach((issue) => {
-            errorMessage += issue.message + ". ";
-         });
-         return { error: errorMessage };
-      }
+    try {
+        const result = UserRegisterSchema.safeParse(form);
+        if (!result.success) {
+            let errorMessage = "";
+            result.error.issues.forEach((issue) => {
+                errorMessage += issue.message + ". ";
+            });
+            return { error: errorMessage };
+        }
 
-      const checkEmail = await db.users.findFirst({
-         where: {
-            email: result.data.email,
-         },
-      });
+        const checkEmail = await db.users.findFirst({
+            where: {
+                email: result.data.email,
+            },
+        });
 
-      if (checkEmail) {
-         return { error: "Email already exists" };
-      }
+        if (checkEmail) {
+            return { error: "Email already exists" };
+        }
 
-      const id = result.data.studentId.substring(0, 3);
+        const id = result.data.studentId.substring(0, 3);
 
-      const checkMajor = await db.major.findFirst({
-         where: {
-            uniqueId: id,
-         },
-      });
+        const checkMajor = await db.major.findFirst({
+            where: {
+                uniqueId: id,
+            },
+        });
 
-      if (!checkMajor) {
-         return {
-            error: "Organization currently not supported, please try to contact us to add organization",
-         };
-      }
+        if (!checkMajor) {
+            return {
+                error: "Major currently not supported, please try to contact us to add major",
+            };
+        }
 
-      const password = await bcrypt.hash(result.data.password, 10);
-      const data = result.data;
-      const putDb = await db.users.create({
-         data: {
-            email: data.email,
-            password: password,
-            firstName: data.firstName,
-            lastName: data.lastName,
-            studentId: data.studentId,
-            majorId: checkMajor.id,
-            yearOfBatch: data.year,
-         },
-      });
-      if (!putDb) {
-         return { error: "Failed to create user" };
-      }
-      return { error: false, message: "User created", id: putDb.id };
-   } catch (err) {
-      return { error: "Failed to create user" };
-   }
+        const password = await bcrypt.hash(result.data.password, 10);
+        const data = result.data;
+        const putDb = await db.users.create({
+            data: {
+                email: data.email,
+                password: password,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                studentId: data.studentId,
+                majorId: checkMajor.id,
+                yearOfBatch: data.year,
+            },
+        });
+        if (!putDb) {
+            return { error: "Failed to create user" };
+        }
+        return { error: false, message: "User created", id: putDb.id };
+    } catch (err) {
+        return { error: "Failed to create user" };
+    }
 }
 
 const MajorSchema = z.object({
-   id: z
-      .string({
-         required_error: "ID is required",
-      })
-      .length(3, {
-         message: "ID must be 3 characters",
-      }),
+    id: z
+        .string({
+            required_error: "ID is required",
+        })
+        .length(3, {
+            message: "ID must be 3 characters",
+        }),
 });
 
 export async function getMajor(formData: unknown) {
-   const result = MajorSchema.safeParse(formData);
-   if (!result.success) {
-      let errorMessage = "";
-      result.error.issues.forEach((issue) => {
-         errorMessage += issue.message + ". ";
-      });
-      return { error: errorMessage };
-   }
+    const result = MajorSchema.safeParse(formData);
+    if (!result.success) {
+        let errorMessage = "";
+        result.error.issues.forEach((issue) => {
+            errorMessage += issue.message + ". ";
+        });
+        return { error: errorMessage };
+    }
 
-   const major = await db.major.findFirst({
-      where: {
-         uniqueId: result.data.id,
-      },
-   });
+    const major = await db.major.findFirst({
+        where: {
+            uniqueId: result.data.id,
+        },
+    });
 
-   if (!major) {
-      return { error: "Major not found" };
-   }
+    if (!major) {
+        return { error: "Major not found" };
+    }
 
-   return { error: false, major: major };
+    return { error: false, major: major };
 }
